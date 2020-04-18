e8886065146c3aa7534d3008e3ad67aab6d1c546
Drew <andrewwiskus@gmail.com>
2020-04-18 01:20:05 -0500

    images

---
+++ src/images/images.ts
@@ -0,0 +1,11 @@
+const beeg = require('./beeg_v1.png');
+const happy_icon = require('./happiness_icon.png');
+const health_icon = require('./health_icon.png');
+const cow = require('./cow.png');
+
+export const images = {
+    beeg: beeg,
+    happy_icon,
+    health_icon,
+    cow,
+};
