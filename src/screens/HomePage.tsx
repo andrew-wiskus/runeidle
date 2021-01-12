05916b763faec4ac61759a0a3c0dbe0a43d95712
Drew <drew.wiskus@space150.com>
2021-01-12 01:19:54 -0600

    added_woodworking

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,11 +1,25 @@
-import { GameTickStore } from 'data/GameTickStore'
-import { inject, observer } from 'mobx-react'
-import React from 'react'
+import { SkillTableHead } from 'components/Skill/SkillTableHead';
+import { SkillTableRow } from 'components/Skill/SkillTableRow';
+import { GameTickStore } from 'data/GameTickStore';
+import { inject, observer } from 'mobx-react';
+import { SKILLS } from 'models/Skill/Skill';
+import { SKILL_PRODUCTIONS } from 'models/Skill/SkillProductions';
+import { SkillID } from 'models/Skill/Skill_ID';
+import React from 'react';
 
 @inject('gameTickStore')
 @observer
-export class HomePage extends React.Component<{gameTickStore?: GameTickStore}, {}> {
-    public render() {
-        return <h1>hello world {this.props.gameTickStore!.currentTick}</h1>
-    }
+export class HomePage extends React.Component<{ gameTickStore?: GameTickStore }, {}> {
+	public render() {
+		return (
+			<div style={{ padding: 50 }}>
+				<h1>hello world {this.props.gameTickStore!.currentTick}</h1>
+
+				<SkillTableHead skill={SKILLS(SkillID.WOODWORKING)} />
+				<SkillTableRow production={SKILL_PRODUCTIONS(SkillID.WOODWORKING)[0]} />
+				<SkillTableRow production={SKILL_PRODUCTIONS(SkillID.WOODWORKING)[1]} />
+				<SkillTableRow production={SKILL_PRODUCTIONS(SkillID.WOODWORKING)[2]} />
+			</div>
+		);
+	}
 }
